b67f8bd54f0561d9cfdd81004bfff583e905c8d2
helog234 <helenegandar@ik.me>
2025-08-07 11:09:20 +0200

    Sync with nfs vm

---
+++ server-3-infra/terraform-proxmox/nfs.tf
@@ -0,0 +1,62 @@
+resource "proxmox_virtual_environment_vm" "nfs-server" {
+  name      = "nfs-server-01"
+  node_name = var.proxmox_node
+  on_boot   = true
+
+  clone {
+    vm_id = var.template_id
+    full  = true
+  }
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = 2
+    type  = "host"
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  disk {
+    interface    = "scsi0"
+    size         = 32
+  }
+
+  boot_order = ["scsi0"]
+
+  initialization {
+    user_account {
+      username = "ubuntu"
+      keys     = [trimspace(file("~/.ssh/id_ed25519.pub"))]
+    }
+
+    ip_config {
+      ipv4 {
+        address = "192.168.2.32/24"
+        gateway = "192.168.2.1"
+      }
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'SSH is ready, waiting for cloud-init to finish...'",
+      "sudo cloud-init status --wait",
+      "echo 'Cloud-init finished!'"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_ed25519")
+      host = self.ipv4_addresses[1][0]
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible-ldap/inventory.ini ../ansible-ldap/nfs.yml"
+  }
+}
